038dbd9a0fe07a476e813ad895b96842dad85a0f
ali <alinosratipour@gmail.com>
2021-07-18 15:12:07 +0100

    resolved the db connection

--- server/db.js
+++ server/db.js
@@ -1,11 +1,18 @@
 import { Pool } from "pg";
-
+import dotenv from "dotenv";
+dotenv.config();
 const dbUrl = process.env.DATABASE_URL || "postgres://localhost:5432/cyf";
 
+console.log({dbUrl});
 const pool = new Pool({
 	connectionString: dbUrl,
 	connectionTimeoutMillis: 5000,
 	ssl: dbUrl.includes("localhost") ? false : { rejectUnauthorized: false },
+	// user:'postgres',
+    // host:'localhost',
+    // database: "devplanner_db",
+    // password:'postgres',
+    // port:5432
 });
 
 export const connectDb = async () => {
