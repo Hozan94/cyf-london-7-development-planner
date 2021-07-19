e286616c20ad925d3b21c6e644b5188a76db0625
Hozan Ali <hozan949@gmail.com>
2021-07-19 14:31:30 +0100

    first commit ro development

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,44 +1,210 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+//import React from 'react'
+//import "./Home.css";
+//import image from '../img/cyf.png';
+//import { useForm } from "react-hook-form";
+//import { yupResolver } from "@hookform/resolvers/yup";
+//import * as yup from "yup";
 
-import "./Home.css";
-import logo from "./logo.svg";
+//const userSchema = yup.object().shape({
+//    firstName: yup.string().required('First Name is required'),
+//    lastName: yup.string().required("Last Name is required"),
+//    city: yup.string().required(),
+//    class: yup.string().required(),
+//    email: yup.string().email("Email is not valid").required(),
+//    password: yup.string().min(8).max(15).required(),
+//    confirmPassword: yup.string().oneOf([yup.ref("password"), null], 'Passwords must match')
 
-export function Home() {
-	const [message, setMessage] = useState("Loading...");
+//});
 
-	useEffect(() => {
-		fetch("/api")
-			.then((res) => {
-				if (!res.ok) {
-					throw new Error(res.statusText);
-				}
-				return res.json();
-			})
-			.then((body) => {
-				setMessage(body.message);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}, []);
+//function Signup() {
 
-	return (
-		<main role="main">
-			<div>
-				<img
-					className="logo"
-					data-qa="logo"
-					src={logo}
-					alt="Just the React logo"
-				/>
-				<h1 className="message" data-qa="message">
-					{message}
-				</h1>
-				<Link to="/about/this/site">About</Link>
-			</div>
-		</main>
-	);
+//    const handleClassMenu = (value) => {
+//        alert(value);
+//    };
+
+//    const handleRoleMenu = (value) => {
+//        alert(value);
+//    }
+
+//    const { register, handleSubmit, formState: { errors } } = useForm({
+//        resolver: yupResolver(userSchema)
+//    });
+
+//    const onSubmit = (data) => {
+//        console.log(data)
+//        fetch("http://localhost:3000/api/graduates", {
+//            method: "POST",
+//            headers: {
+//                "Content-Type": "application/json",
+//            },
+//            body: JSON.stringify(data),
+//        })
+//            .then((response) => response.json())
+//            .then((data) => {
+//                console.log(data);
+//            })
+//            .catch((error) => {
+//                console.error(error);
+//            });
+//    };
+
+//    return (
+//        <div>
+//            <div className="signUp-container">
+//                <img src={image} alt="cyf-logo" />
+//                <form onSubmit={handleSubmit(onSubmit)}>
+//                    <div className="user-details">
+                        
+//                        <div className="input-box">
+//                            <span className="details">First Name</span>
+//                            <input type="text"   {...register("firstName")} />
+                           
+//                            <p>{errors.firstName?.message}</p>
+//                        </div>
+
+//                        <div className="input-box">
+//                            <span className="details">Last Name</span>
+//                            <input type="text"   {...register("lastName")} />
+
+//                            <p>{errors.lastName?.message}</p>
+//                        </div>
+
+//                        <div className="input-box">
+//                            <span className="details">Class</span>
+//                            <select className="role" name="studentClass" id="studentClass" required
+//                                {...register("class")}
+//                                onChange={(e) => handleClassMenu(e.target.value)}
+//                            >
+//                                <option value="">--Please choose your Class--</option>
+//                                <option value="WMS01">WMS01</option>
+//                                <option value="WMS02">WMS02</option>
+//                                <option value="LDN06">LDN06</option>
+//                                <option value="LDN07">LDN07</option>
+//                            </select>
+//                        </div>
+
+//                        <div className="input-box">
+//                            <span className="details">Role</span>
+//                            <select className="role" name="role" id="role" required
+//                                {...register("role")}
+//                                onChange={(e) => handleRoleMenu(e.target.value)}
+//                            >
+//                                <option value="">--Please choose your Role--</option>
+//                                <option value="graduate">Graduate</option>
+//                                <option value="mentor">Mentor</option>
+//                            </select>
+//                        </div>
+
+//                        <div className="input-box">
+//                            <span className="details">City</span>
+//                            <input type="text"   {...register("city")} />
+
+//                            <p>{errors.city?.message}</p>
+//                        </div>
+
+//                        <div className="input-box">
+//                            <span className="details">Email</span>
+//                            <input type="email"   {...register("email")} />
+
+//                            <p>{errors.email?.message}</p>
+//                        </div>
+
+//                        <div className="input-box">
+//                            <span className="details">Password</span>
+//                            <input type="password"    {...register("password")} />
+
+//                            <p>{errors.password?.message}</p>
+//                        </div>
+
+//                        <div className="input-box">
+//                            <span className="details">Confirm Password</span>
+//                            <input type="password"   {...register("confirmPassword")} />
+                            
+//                            <p>{errors.confirmPassword?.message}</p>
+//                        </div>
+                    
+//                    </div>
+//                    <div className="btn-holder">
+//                        <button className="btn-signup">Sign up</button>
+//                    </div>
+//                </form>
+//            </div>
+//        </div>
+//    )
+//}
+
+//export default Signup;
+import 'bootstrap/dist/css/bootstrap.css';
+import { Tabs, Tab, Container, Row, Col } from 'react-bootstrap';
+import SignUp from './singUp/SignUp';
+import Login from './Login/Login';
+
+function Home(){
+   return(
+        //<Container>
+        //    <Row>
+        //        <Col>
+        <Tabs defaultActiveKey="SignUp" id="uncontrolled-tab-example" className="mb-3">
+            <Tab eventKey="signUp" title="Signup">
+                <SignUp />
+            </Tab>
+            <Tab eventKey="login" title="Login">
+                <Login />
+            </Tab>
+        </Tabs>
+        //        </Col>
+        //    </Row>
+        //</Container>
+    );
 }
 
 export default Home;
+
+
+
+
+
+//import { useEffect, useState } from "react";
+//import { Link } from "react-router-dom";
+
+//import "./Home.css";
+//import logo from "./logo.svg";
+
+//export function Home() {
+//	const [message, setMessage] = useState("Loading...");
+
+//	useEffect(() => {
+//		fetch("/api")
+//			.then((res) => {
+//				if (!res.ok) {
+//					throw new Error(res.statusText);
+//				}
+//				return res.json();
+//			})
+//			.then((body) => {
+//				setMessage(body.message);
+//			})
+//			.catch((err) => {
+//				console.error(err);
+//			});
+//	}, []);
+
+//	return (
+//		<main role="main">
+//			<div>
+//				<img
+//					className="logo"
+//					data-qa="logo"
+//					src={logo}
+//					alt="Just the React logo"
+//				/>
+//				<h1 className="message" data-qa="message">
+//					{message}
+//				</h1>
+//				<Link to="/about/this/site">About</Link>
+//			</div>
+//		</main>
+//	);
+//}
+
+//export default Home;
--- server/api.js
+++ server/api.js
@@ -1,9 +1,179 @@
 import { Router } from "express";
+import { Pool } from "pg";
 
 const router = new Router();
 
 router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
 });
+
+const dbUrl = process.env.DATABASE_URL || "postgres://localhost:5432/cyf";
+
+const pool = new Pool({
+    connectionString: dbUrl,
+});
+
+//GET all "/graduates" OR get a graduate with a searchTerm included in their details "/graduates?q={searchTerm}"
+router.get("/graduates", (req, res) => {
+    const searchTerm = req.query.q;
+
+    if (searchTerm) {
+        //Get graduates when searchTerm is included in either last_name, first_name or class
+        const query =
+
+            `SELECT graduates.id, first_name, last_name, classes.class_code, TO_CHAR(sign_up_date:: DATE, 'yyyy-mm-dd') AS sign_up_date FROM graduates INNER JOIN classes ON graduates.class_id = classes.id WHERE first_name ILIKE '%${searchTerm}%' OR last_name ILIKE '%${searchTerm}%' OR classes.class_code ILIKE '%${searchTerm}%'`;
+
+        pool
+            .query(query)
+            .then((result) => {
+                if (result.rowCount) {
+                    res.json({ "graduates": result.rows });
+                } else {
+                    res.status(404).json({ "status": 404, "error": "No graduates found" });
+                }
+            })
+            .catch((error) => console.log(error));
+
+    } else {
+        //Get all graduates
+        const query =
+
+            "SELECT graduates.id, first_name, last_name, classes.class_code, TO_CHAR(sign_up_date:: DATE, 'yyyy-mm-dd') AS sign_up_date FROM graduates INNER JOIN classes ON graduates.class_id = classes.id ORDER BY graduates.id";
+
+        pool
+            .query(query)
+            .then((result) => res.json({ "graduates": result.rows }))
+            .catch((error) => console.log(error));
+    }
+});
+
+
+//GET a graduate with a specified ID at "/graduates/:id"
+router.get("/graduates/:id", (req, res) => {
+    const graduateId = req.params.id;
+
+    const query =
+
+        "SELECT graduates.id, first_name, last_name, classes.class_code, TO_CHAR(sign_up_date:: DATE, 'yyyy-mm-dd') AS sign_up_date FROM graduates INNER JOIN classes ON graduates.class_id = classes.id WHERE graduates.id = $1";
+
+    pool
+        .query(query, [graduateId])
+        .then((result) => {
+            if (result.rowCount) {
+                res.json(result.rows);
+            } else {
+                res.status(404).json({ "status": 404, "error": "No graduate found" });
+            }
+        })
+        .catch((error) => console.log(error));
+});
+
+
+//POST a new graduate at "/graduates"
+router.post("/graduates", (req, res) => {
+    const graduateFirstName = req.body.firstName;
+    const graduateLastName = req.body.lastName;
+    const graduateEmail = req.body.email;
+    const graduatePassword = req.body.password;
+    const graduateClass = req.body.class;
+    let graduateClassId;
+
+    graduateClass === "LDN06" ? graduateClassId = 1 : graduateClass === "WMS01" ? graduateClassId = 2 :
+    graduateClass === "LDN07" ? graduateClassId = 3 : graduateClass === "WMS02" ? graduateClassId = 4 : null;
+
+    //Validate all fields are filled in
+    if (!graduateFirstName || !graduateLastName || !graduateEmail || !graduatePassword || !graduateClassId) {
+        return res.status(400).json({ "status": 400, "error": "Please fill in all fields" });
+    }
+
+    //Validate graduate does not exist (using email and password)
+    const query =
+
+        "SELECT first_name FROM graduates WHERE email=$1";
+
+    pool
+        .query(query, [graduateEmail])
+        .then((result) => {
+            if (result.rowCount) {
+                res.status(400).json({ "status": 400, "error": "An account already exist" });
+            } else {
+                const query =
+
+                    "INSERT INTO graduates ( first_name, last_name, email, password, class_id) VALUES ($1, $2, $3, $4, $5) RETURNING id, first_name, last_name, class_id, TO_CHAR(sign_up_date:: DATE, 'yyyy-mm-dd') AS sign_up_date";
+
+                pool
+                    .query(query, [graduateFirstName, graduateLastName, graduateEmail, graduatePassword, graduateClassId])
+                    .then((result) => res.json({ "success": "New graduate is created", "graduate": result.rows }))
+                    .catch((e) => console.error(e));
+            }
+        });
+});
+
+
+//UPDATE a graduate detail with a specified Id
+router.put("/graduates/:id", (req, res) => {
+    const graduateId = req.params.id;
+
+    const query =
+
+        "SELECT id, first_name, last_name, email, password, class_id FROM graduates WHERE id = $1";
+
+    pool
+        .query(query, [graduateId])
+        .then((result) => {
+            if (result.rowCount === 0) {
+                res.status(404).json({ "status": 404, "error": "No graduate found" });
+            } else {
+                const graduateFirstName = req.body.firstName;
+                const graduateLastName = req.body.lastName;
+                const graduateEmail = req.body.email;
+                const graduatePassword = req.body.password;
+                const graduateClass = req.body.class;
+
+                let graduateClassId;
+
+                graduateClass === "LDN06" ? graduateClassId = 1 : graduateClass === "WMS01" ? graduateClassId = 2 :
+                graduateClass === "LDN07" ? graduateClassId = 3 : graduateClass === "WMS02" ? graduateClassId = 4 : null;
+
+                let currentFirstName = result.rows[0].first_name;
+                let currentLastName = result.rows[0].last_name;
+                let currentEmail = result.rows[0].email;
+                let currentPassword = result.rows[0].password;
+                let currentClassId = result.rows[0].class_id;
+
+                const query =
+
+                    "UPDATE graduates SET first_name=$1, last_name=$2, email=$3, password=$4, class_id=$5 WHERE id=$6 RETURNING id, first_name, last_name, class_id, TO_CHAR(sign_up_date:: DATE, 'yyyy-mm-dd') AS sign_up_date";
+
+                pool
+                    .query(query, [graduateFirstName || currentFirstName, graduateLastName || currentLastName, graduateEmail || currentEmail, graduatePassword || currentPassword, graduateClassId || currentClassId, graduateId])
+                    .then((result) => res.json({ "success": "Graduate details updated", "graduate": result.rows }))
+                    .catch((e) => console.error(e));
+            }
+        })
+        .catch((e) => console.error(e));
+});
+
+
+//DELETE a graduate with a specified Id at "/graduates/:id"
+router.delete("/graduates/:id", (req, res) => {
+    const graduateId = req.params.id;
+
+    const query =
+
+        "DELETE FROM graduates WHERE id=$1 RETURNING first_name , last_name";
+
+    pool
+        .query(query, [graduateId])
+        .then((result) => {
+            if (result.rowCount) {
+                res.json({ "success": "Graduate is deleted" });
+            } else {
+                res.status(404).json({ "status" : 404, "error" : "No graduate found" });       
+            } 
+        })
+        .catch((e) => console.error(e));
+});
+
 
 export default router;
