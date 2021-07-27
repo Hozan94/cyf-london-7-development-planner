0f079edfa202443840807a48af51fb8be8e45284
Hozan <74840726+Hozan94@users.noreply.github.com>
2021-07-27 23:24:38 +0100

    Revert "Hozan94 development"

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,7 @@ import { Route, Switch } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
-import GraduateBoard from "./pages/GraduateBoard";
-//here I made some comment
-//This is also Hozan comment
+
 const App = () => (
 	<Switch>
 		<Route path="/" exact>
@@ -13,9 +11,6 @@ const App = () => (
 		<Route path="/about/this/site">
 			<About />
 		</Route>
-        <Route path="/graduate/board">
-            <GraduateBoard />
-        </Route>
 	</Switch>
 );
 
