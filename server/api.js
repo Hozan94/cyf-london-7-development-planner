18a34dc9ea8da93ab7524504476f4e431c580482
BenyamWorku <benworku@gmail.com>
2021-08-02 23:25:31 +0100

    /graduates/:graduate_id/plans/goals

--- server/api.js
+++ server/api.js
@@ -488,6 +488,8 @@ router.get("/graduates/:graduate_id/plans", (req, res) => {
 		.catch((e) => console.error(e));
 });
 router.get("/graduates/:graduate_id/:plan_id([0:9])", (req, res) => {
+   
+
 	const graduateId = req.params.graduate_id;
 	const planId = req.params.plan_id;
 	const query = "SELECT * FROM plans WHERE graduate_id=$1 and id=$2";
@@ -557,61 +559,12 @@ router.get("/graduates/:graduate_id/goals", (req, res) => {
 		})
 		.catch((e) => console.error(e));
 });
-// adding plans and goals post requests
-// router.post("/graduates/:graduate_id/plans/goals", (req, res) => {
-// 	const { plan_name, goals_list } = req.body; // this is found in the fetch body
-// 	const graduateId = req.params.graduate_id; // this is found in the fetch url
-// 	// const goals_list=[{goal_details:"text1"},{goal_details:"text2"}]
-// 	//Validate all fields are filled in
-// 	if (!plan_name || !graduateId) {
-// 		return res.status(400).json({
-// 			status: 400,
-// 			error: "This api endpoint requires plan name and graduate id",
-// 		});
-// 	}
 
-// 	const query = "SELECT * FROM plans WHERE plan_name=$1 and graduate_id=$2";
 
-//     pool.query(query, [plan_name, graduateId])
-//         .then((result) => {
-//             if (result.rowCount) {
-//                 res.status(400).json({
-//                     status: 400,
-//                     error:
-//                         "Please change the plan name, this graduate has already got a plan with the same name",
-//                 });
-//             } else {
-//                 const query_plan =
-//                     "INSERT INTO plans (plan_name,graduate_id) VALUES ($1,$2) RETURNING id";
 
-//                 pool
-//                     .query(query_plan, [plan_name, graduateId])
-//                     .then((result) => {
-//                         goals_list
-//                             .map((item) => {
-//                                 console.log(item);
-//                                 // item.goal_details
-//                                 const query_goals =
-//                                     "INSERT INTO goals (plan_id,goal_details,goal_status_id) VALUES($1,$2,$3)";
-//                                 pool.query(query_goals, [
-//                                     result.rows[0].id,
-//                                     item.goal_details,
-//                                     1,
-//                                 ]);
-//                             })
 
-//                             .then(() => {
-//                                 res.json({
-//                                     success: "All the goals are saved ",
-//                                 });
-//                             })
-//                             .catch((e) => console.log(e));
-//                     })
-//                     .catch((e) => console.error(e));
-//             }
-//         })
-//         .catch((e) => console.log(e));
-// });
+
+
 router.post("/graduates/:graduate_id/plans/goals", (req, res) => {
     const { plan_name, goals_list } = req.body; // this is found in the fetch body
     const graduateId = req.params.graduate_id; // this is found in the fetch url
@@ -664,7 +617,22 @@ router.post("/graduates/:graduate_id/plans/goals", (req, res) => {
     .catch((e) => console.error(e));
 });
 
+router.get("/graduates/:graduate_id/plans/goals", async (req, res) => {
 
+      try {
+				
+				const graduate_plans = await pool.query(
+                    `select plans.id,plans.plan_name ,
+                    json_agg(json_build_object('goal_details',goals.goal_details,'due_date',goals.due_date,'remarks',goals.remarks))
+                     goals_list from goals inner join plans on plans.id=goals.plan_id where graduate_id=$1 group by 1,2 ;`,
+					[req.params.graduate_id]
+				);
+				res.json(graduate_plans.rows);
+			} catch (err) {
+				res.status(500).send("server error");
+			}
+
+});
 
 /*feedacks endpoints */
 
