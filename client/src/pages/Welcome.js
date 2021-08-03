f775d5df4c2d2478d52bac49185052e1b60cbf92
BenyamWorku <benworku@gmail.com>
2021-08-03 23:27:10 +0100

    made no changes

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -4,7 +4,7 @@ function Welcome() {
     return (
 			<div>
 				<h1>Development Planner</h1>
-				<h2>If you are new here Sign up.Log in otherwise</h2>
+				<h2>If you are new here Sign up.Log in otherwise.</h2>
 			</div>
 		);
 }
